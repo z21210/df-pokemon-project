6fc1b8e723989f9ed5f369fc7cb2190bff9d0b40
Daniel Clarke <clarked311@hotmail.co.uk>
2025-04-29 14:06:52 +0100

    implemented selection

--- src/main.py
+++ src/main.py
@@ -6,52 +6,6 @@ st.title("Streamlit Pokemon Project")
 # Read in CSV
 df = pd.read_csv("data/pokemon.csv")
 
-# Data Display
-st.write("OG Dataframe")
-df
-
-# Summary Stats found not useful outside of data analysis
-# st.write(df.describe())
-
-'''markdown
-###
-
-# copied in from demo 5 to implement filters
-# INSERT sidebar code here:
-# Add a sidebar for filters
-st.sidebar.header("Filters")
-# Passenger Class filter
-pclass_filter = st.sidebar.multiselect(
-    "Select Passenger Class",
-    options=sorted(df["Pclass"].unique()),
-    default=sorted(df["Pclass"].unique()),
-)
-# Embarked Port filter
-embarked_filter = st.sidebar.multiselect(
-    "Select Embarked Port",
-    options=sorted(df["Embarked"].unique()),
-    default=sorted(df["Embarked"].unique()),
-)
-
-# END INSERT sidebar
-
-# MODIFY CODE BELOW WITH filtered_df
-# Total Passengers
-# total_passengers = df["PassengerId"].nunique()
-total_passengers = filtered_df["PassengerId"].nunique()
-# Overall Survival Rate
-# overall_survival_rate = df["Survived"].mean()
-overall_survival_rate = filtered_df["Survived"].mean()
-# Average Age
-# average_age = df["Age"].mean()
-average_age = filtered_df["Age"].mean()
-# Survival Rate by Gender
-# survival_rate_by_gender = df.groupby("Sex", observed=False)["Survived"].mean()
-survival_rate_by_gender = filtered_df.groupby("Sex", observed=False)[
-    "Survived"
-].mean()
-# END MODIFY df
-### '''
 number = st.number_input("Choose a Pokedex Number", 1, 898, placeholder="Please enter a number between 1 and 898")
 
 st.write("Current pokedex number is ", number)
@@ -61,24 +15,35 @@ filtered_df = df[
     (df["pokedex_number"] == number)
 ]
 
-filtered_df
 names = filtered_df["name"].values
 
+variant = st.segmented_control("Variations", filtered_df["name"].values, default = names[0])
+
+selected = filtered_df[
+    (filtered_df["name"] == variant)
+]
+
+selected
+
 st.header(names[0])
-st.write("Pokedex Number #", filtered_df["pokedex_number"])
+st.write("Pokedex Number #", selected["pokedex_number"])
 st.image(f"https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/{number:03}.png", width=100)
-st.caption(filtered_df["japanese_name"].values[0])
-st.metric(label="HP", value=filtered_df["hp"].values[0])
+st.caption(selected["japanese_name"].values[0])
+st.metric(label="HP", value=selected["hp"].values[0])
 st.markdown(
     "Type  \n"
     ":green-badge[:material/grass: Grass] :violet-badge[Poison]"
 )
-st.segmented_control("Variations", filtered_df["name"].values)
+
 
 st.divider()
 st.header("Components")
 
 st.write("Pokemon Name")
+st.write("Abilities")
+abilities1, abilities2 = st.columns(2)
+st.pills("Tags", ["Type 1", "Type 2"])
+st.badge("Type 1", color="green")
 st.segmented_control("Variations", filtered_df["name"].values)
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("HP", 60, border=True)
@@ -86,6 +51,3 @@ col2.metric("A metric", 40, "something", delta_color="off")
 col3.metric("A metric", 41, 2, border=True)
 col4.metric("Another", 2)
 st.divider()
-
-with col1:
-    st.metric(label="HP", value=HP)
