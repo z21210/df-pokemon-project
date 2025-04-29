ac4a7571c7186864a1958052af344ce095348797
z21210 <z21210@protonmail.com>
2025-04-29 14:54:48 +0100

    finished data exploration

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv('data/pokemon.csv')
-
 df.set_index('pokedex_number')
 
+## Exploration
 # get which columns contain nulls
 df.columns[df.isna().any()]
 # 'type_2', 'weight_kg', 'ability_1', 'ability_2', 'ability_hidden', 'catch_rate', 'base_friendship', 'base_experience', 'growth_rate', 'egg_type_1', 'egg_type_2', 'percentage_male', 'egg_cycles'
@@ -20,7 +20,7 @@ assert all(df['abilities_number'] == 1*df['ability_1'].notna() + 1*df['ability_2
 
 # check what has null catch_rate
 print(df[df['catch_rate'].isna()])
-# Galarians should have catch_rate equivalent to their normal variants; data transformation needed
+# Galarians should have catch_rate equivalent to their base variants; data transformation needed
 
 # check what has null base_friendship
 print(df[df['base_friendship'].isna()])
@@ -39,9 +39,12 @@ assert len(df[df['type_number'] != 1*df['type_1'].notna() + 1*df['type_2'].notna
 
 # percentage_male
 print(df[df['percentage_male'].isna()])
-# Galarians should have percentage_male equivalent to their normal variants; data transformation needed
+# Galarians should have percentage_male equivalent to their base variants; data transformation needed
 # Others are genderless; appropriate nulls
 
-# check what has null percentage_male
-print(df[df['percentage_male'].isna()])
-# Galarian Darmanitan Zen Mode has percentage_male as null for lore reasons; null is appropriate
+## Transformation
+# base_friendship and base_experience have bad nulls, drop columns
+df.drop(columns=['base_friendship', 'base_experience'], inplace=True)
+
+# Galarians should have catch_rate and percentage_male equivalent to their base variants
+# this is handled in the streamlit code
