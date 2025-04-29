8315811bde11c14e180a2b445e2c5a71cd78c3a2
Xiangnan Liu <150020746+emilyliu75@users.noreply.github.com>
2025-04-29 16:02:40 +0100

    adding search by name

--- src/main.py
+++ src/main.py
@@ -11,6 +11,11 @@ def get_pokemon(df, number):
         (df["pokedex_number"] == number)
     ]
 
+def get_pokemon_by_name(df, name):
+    return df[
+        (df["name"].str.contains(name, case=False, na=False))
+    ]
+
 def get_variant(df, variant):
     return df[
         (df["name"] == variant)
@@ -34,13 +39,23 @@ df = load_pokemon()
 st.title("Streamlit Pokemon Project")
 
 number = st.number_input("Choose a Pokedex Number", 1, 898, placeholder="Please enter a number between 1 and 898")
+search_name = st.text_input("Search by Pokémon Name (partial match allowed)")
+if search_name:
+    filtered_df = get_pokemon_by_name(df, search_name)
+    if not filtered_df.empty:
+        variant = st.selectbox("Select Variation", filtered_df["name"].values)
+        number = filtered_df["pokedex_number"].values[0]
+        selected = get_variant(filtered_df, variant)
+        display_metrics(selected, variant)
+
 
 # Filter the DataFrame based on the selected filters
-filtered_df = get_pokemon(df, number)
-names = filtered_df["name"].values
+else:
+    filtered_df = get_pokemon(df, number)
+    names = filtered_df["name"].values
 
-variant = st.segmented_control("Variations", filtered_df["name"].values, default = names[0])
-selected = get_variant(filtered_df, variant)
+    variant = st.segmented_control("Variations", filtered_df["name"].values, default = names[0])
+    selected = get_variant(filtered_df, variant)
 
-display_metrics(selected, variant)
+    display_metrics(selected, variant)
 
